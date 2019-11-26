b96a230f36349a2e9ee0e9dfeffb64061ea99aa0
Shashank K Bhat <shashank.kakunje@gmail.com>
2019-11-26 20:50:02 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { RouterModule } from '@angular/router';
+import { allAppRoutes } from './routes';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {OpenWeatherService} from './open-weather.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(allAppRoutes),
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [OpenWeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import {OpenWeatherService} from '../open-weather.service';
+
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent implements OnInit {
+  public weatherSearchForm: FormGroup;
+  public weatherData: any
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private weatherService: OpenWeatherService
+    ) { }
+
+  ngOnInit() {
+    this.weatherSearchForm = this.formBuilder.group({
+      location: ['']
+    });
+  }
+
+  sendToOpenWeather(formValues){
+    this.weatherService.getWeather(formValues.location).subscribe(data => this.weatherData = data)
+    console.log(this.weatherData);
+  }
+
+}
